fb9e0079525d04a409fb475b33dc306913f7ac54
Supakorn <sptbluesteamxv@hotmail.com>
2020-12-23 02:36:48 +0700

    no message

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
+const scgs = require('./db')
 
+app.get('/scgs', (req, res) => {
+    res.json(scgs)
+})
+
+//find X, Y, Z 
+app.get('/scgs/:id', (req, res) => {
+    res.json(scgs.find(scgs => scgs.id === req.params.id))
+})
+  
+//called “DOSCG
 app.get('/', (req, res) => {
   res.send('DOSCG')
-
-//   res.send('Hello Team')
 })
 
 app.listen(3000, () => {
   console.log('Start server at port 3000.')
 })
+
